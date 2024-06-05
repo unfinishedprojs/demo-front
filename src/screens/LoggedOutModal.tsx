74dcc25e958052b82e4fc1e31fdc5c29b2d758e5
KraXen72 <dpelech1@gmail.com>
2024-06-05 21:26:18 +0200

    add discord id input

--- src/screens/LoggedOutModal.tsx
+++ src/screens/LoggedOutModal.tsx
@@ -1,28 +1,50 @@
 import { Button, TextField } from "@suid/material";
+import { createSignal } from "solid-js";
+import styles from './LoggedOutModal.module.css';
+
+
+const [userToken, setUserToken] = createSignal('');
+const [inviteCode, setInviteCode] = createSignal('');
+
+function login() {
+	if (userToken() === '') return;
+
+}
 
 
 export default function LoggedOutScreen() {
-	return (<div class="w-auto flex flex-col justify-center gap-3
+	return (<div class="w-auto flex flex-col justify-center gap-5
 		p-8 bg-slate-300 rounded-lg
 	">
-		<h1 class="text-lg font-medium">Democracy Server Voting Infra</h1>
+		<h1 class="text-2xl font-600">Democracy Server Voting Infra</h1>
+		<h2 class="text-md font-medium">Login</h2>
 		<div class="flex gap-2 flex-col items-end">
 			<TextField
         id="outlined-basic"
         label="User token"
         variant="outlined"
-				class="w-96"
+				fullWidth
+				onChange={(e) => setUserToken(e.currentTarget.value)}
       />
 			<Button variant="contained" class="w-36" >Login</Button>
 		</div>
-		<div class="flex gap-2 flex-col items-end">
+		<h2 class="text-md font-medium">Register</h2>
+		<div class={`${styles.registerWrapper}`}>
 			<TextField
         id="outlined-basic"
-        label="Account creation invite code"
+        label="Registration code"
         variant="outlined"
-				class="w-96"
+				style={{ 'grid-area': 'codeInput' }}
+				onChange={(e) => setInviteCode(e.currentTarget.value)}
       />
-			<Button variant="contained" class="w-36">Register</Button>
+			<TextField
+        id="outlined-basic"
+        label="Discord ID"
+        variant="outlined"
+				style={{ 'grid-area': 'discordIDInput' }}
+      />
+			<Button variant="contained" 
+				class="w-36" style={{ 'grid-area': 'button' }}>Register</Button>
 		</div>
 	</div>)
 }
